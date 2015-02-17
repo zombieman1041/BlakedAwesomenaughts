4a9d555246ccaf841f4d915c7397b5e88fffd9e6
Blake Dayman <daymanblake@gmail.com>
2015-02-17 11:41:03 -0800

    fixed ally creep need to switch which way it walks and where it spawns

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -290,7 +290,7 @@ game.MyCreep = me.Entity.extend({
 		this.now = new Date().getTime();
 		this.body.vel.x -= this.body.accel.x * me.timer.tick;
 		me.collision.check(this, true, this.collideHandler.bind(this), true);
-		this.body.update(delta);//updates the isKeyPressed()
+		this.body.update(delta);
 
 		this._super(me.Entity, "update", [delta]);
 		return true;
@@ -298,11 +298,10 @@ game.MyCreep = me.Entity.extend({
 	},
 
 	collideHandler: function(response)	{
-		if(response.b.type==='EnemyBaseEntity'){
+		if(response.b.type==='PlayerBase'){
 			this.attacking = true;
 			//this.lastAttacking = this.now;
 			this.body.vel.x = 0;
-			//keeps moving the creep to the right to maintain its position
 			this.pos.x = this.pos.x +1;
 			//checks that it has been at least 1 second since this creep hit a base
 			if((this.now-this.lastHit >= 1000)){
