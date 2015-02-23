3065090df25aea314e9cac56af5a8d16be4ad7d3
Blake Dayman <daymanblake@gmail.com>
2015-02-23 13:29:13 -0800

    finished video 27 game freezes when ally creep collides with enemy base. enemy creeps die when colliding with player

--- js/screens/title.js
+++ js/screens/title.js
@@ -5,6 +5,23 @@ game.TitleScreen = me.ScreenObject.extend({
 	onResetEvent: function() {	
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
 		
+		me.input.bindKey(me.input.KEY.ENTER, "start");
+		me.game.world.addChild(new (me.Renderable.extend({
+			init: function(){
+				this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+				this.font = new me.Font("Arial", 46, "white");
+			},
+			draw: function(renderer){
+				this.font.draw(renderer.getContext(), "Awsomenaughts", 450, 130);
+				this.font.draw(renderer. getContext(), "Press Enter to Play", 250, 530);
+			}
+		})));
+
+		this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
+			if(action === "start"){
+				me.state.change(me.state.PLAY);
+			}
+		});
 	},
 	
 	
@@ -12,6 +29,7 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		; // TODO
+		me.input.unbindKey(me.input.KEY.ENTER); // TODO
+		me.event.unsubscribe(this.handler);
 	}
 });
