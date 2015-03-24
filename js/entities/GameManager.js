af680aab679d6b686c5f44696a2621036663b3f6
Blake Dayman <daymanblake@gmail.com>
2015-03-24 11:41:18 -0700

    finished video 48 set up buy text but error when pressing B. setVelocity is undefined

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -104,12 +104,46 @@ game.SpendGold = Object.extend({
 		game.data.buyscreen.updateWhenPaused = true;
 		game.data.buyscreen.setOpacity(0.8);
 		me.game.world.addChild(game.data.buyscreen, 34);
+
 		game.data.player.body.setVelocity(0, 0);
+		
+		me.input.bindKey(me.input.KEY.F1,"F1", true);
+		me.input.bindKey(me.input.KEY.F2,"F2", true);
+		me.input.bindKey(me.input.KEY.F3,"F3", true);
+		me.input.bindKey(me.input.KEY.F4,"F4", true);
+		me.input.bindKey(me.input.KEY.F5,"F5", true);
+		me.input.bindKey(me.input.KEY.F6,"F6", true);
+		this.setBuyText();										
+	},
+
+	setBuyText: function(){
+		game.data.buytext = new (me.Renderable.extend({
+			init: function(){
+				this._super(me.Renderable, 'init', [game.data.pausePos.x, game.data.pausePos.y, 300, 50]);
+				this.font = new me.Font("Arial", 26, "black");
+				this.updateWhenPaused = true;
+				this.alwaysUpdate = true;
+			},
+			draw: function(renderer){
+				this.font.draw(renderer.getContext(), "PRESS F1-F6 TO BUY, B TO EXIT", this.pos.x, this.pos.y);
+				
+			
+			}
+
+		}));
+		me.game.world.addChild(game.data.buytext, 35);
 	},
 	stopBuying: function(){
 		this.buying = false;
 		me.state.resume(me.state.PLAY);
 		game.data.player.body.setVelocity(game.data.playerMoveSpeed, 20);
 		me.game.world.removeChild(game.data.buyscreen);
+		me.input.unbindKey(me.input.KEY.F1,"F1", true);
+		me.input.unbindKey(me.input.KEY.F2,"F2", true);
+		me.input.unbindKey(me.input.KEY.F3,"F3", true);
+		me.input.unbindKey(me.input.KEY.F4,"F4", true);
+		me.input.unbindKey(me.input.KEY.F5,"F5", true);
+		me.input.unbindKey(me.input.KEY.F6,"F6", true);		
+		me.game.world.removeChild(game.data.buytext);
 	}
 });
--- js/game.js
+++ js/game.js
@@ -30,7 +30,8 @@ var game = {
 		exp4:0,
 		win: "",
 		pausePos: "",
-		buyscreen: ""
+		buyscreen: "",
+		buytext: ""
 	},
 	
 	
